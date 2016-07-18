9e273ac9db60a35e238caec32bf0cbb255aa19ec
hadesyi <hadesleeq@gmail.com>
2016-07-19 08:39:23 +0900

    Example Domain

---
+++ src/lambda_expressions/chapter1/MusicChapter.java
@@ -0,0 +1,28 @@
+package lambda_expressions.chapter1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Project: java8-lambdas
+ * FileName: MusicChapter
+ * Date: 2016-07-19
+ * Time: 오전 8:38
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class MusicChapter {
+    protected final List<Artist> artists;
+    protected final List<Album> albums;
+
+    public MusicChapter() {
+        artists = new ArrayList<>();
+        albums = new ArrayList<>();
+        loadData("");
+    }
+
+    private void loadData(String file) {
+
+    }
+}
