d25b1c05527ed53288c8938f8921b73eaa3006d8
hadesyi <hadesleeq@gmail.com>
2016-07-20 08:41:50 +0900

    ArrayExample

---
+++ src/lambda_expressions/chapter2/ArrayExample.java
@@ -0,0 +1,16 @@
+package lambda_expressions.chapter2;
+
+/**
+ * Project: java8-lambdas
+ * FileName: ArrayExample
+ * Date: 2016-07-20
+ * Time: 오전 8:40
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class ArrayExample {
+    // BEGIN ARRAY_INFERENCE
+    final String[] array = {"hello", "world"};
+    // END ARRAY_INFERENCE
+}
