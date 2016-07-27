56fb1573581f3b8cef0f92ed306aabc27c95e83d
hadesyi <hadesleeq@gmail.com>
2016-07-28 08:32:09 +0900

    stream exercises

--- src/lambda_expressions/chapter3/StreamExercises.java
+++ src/lambda_expressions/chapter3/StreamExercises.java
@@ -2,7 +2,9 @@ package lambda_expressions.chapter3;
 
 import lambda_expressions.chapter1.Artist;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Stream;
 
 /**
@@ -19,6 +21,23 @@ public class StreamExercises {
         // END COUNT_MEMBERS_EXTERNAL
 
         return totalMembers;
+    }
+
+    // map f = foldr ((:) . f) []
+    // Advanced Exercise
+    public static <T, R> List<R> map(Stream<T> stream, Function<T, R> mapper) {
+        return stream.reduce(new ArrayList<>(), (acc, value) -> {
+            // Make copy of list (modifying acc would violate contract of reduce method)
+            ArrayList<R> result = new ArrayList<>();
+            result.addAll(acc);
+            result.add(mapper.apply(value));
+            return result;
+        }, (left, right) -> {
+            ArrayList<R> result = new ArrayList<>();
+            result.addAll(left);
+            result.addAll(right);
+            return result;
+        });
     }
 
 }
