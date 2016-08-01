72f65a925cd71ff2959f0fbfc29b909d2bf2b870
hadesyi <hadesleeq@gmail.com>
2016-08-02 08:41:35 +0900

    multiple inheritance

--- src/lambda_expressions/chapter4/Carriage.java
+++ src/lambda_expressions/chapter4/Carriage.java
@@ -1,14 +1,5 @@
 package lambda_expressions.chapter4;
 
-/**
- * Project: java8-lambdas
- * FileName: Carriage
- * Date: 2016-07-29
- * Time: 오전 8:51
- * Author: Hadeslee
- * Note:
- * To change this template use File | Settings | File Templates.
- */
 public interface Carriage {
     public default String rock() {
         return "... from side to side";
