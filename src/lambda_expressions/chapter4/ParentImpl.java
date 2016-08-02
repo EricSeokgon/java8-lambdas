624e8110b7e76540a6332eb43ed343d6beea60d0
hadesyi <hadesleeq@gmail.com>
2016-08-03 08:50:28 +0900

    multiple inheritance

---
+++ src/lambda_expressions/chapter4/ParentImpl.java
@@ -0,0 +1,25 @@
+package lambda_expressions.chapter4;
+
+/**
+ * Project: java8-lambdas
+ * FileName: ParentImpl
+ * Date: 2016-08-03
+ * Time: 오전 8:48
+ * Author: user
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class ParentImpl implements Parent {
+
+    private String body;
+
+    @Override
+    public void message(String body) {
+
+    }
+
+    @Override
+    public String getLastMessage() {
+        return null;
+    }
+}
