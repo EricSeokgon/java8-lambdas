5182c89a566b0dc8f50b908da78aada5c2b93c87
hadesyi <hadesleeq@gmail.com>
2016-08-04 08:41:19 +0900

    multiple inheritance

---
+++ src/lambda_expressions/chapter4/ChildImpl.java
@@ -0,0 +1,13 @@
+package lambda_expressions.chapter4;
+
+/**
+ * Project: java8-lambdas
+ * FileName: ChildImpl
+ * Date: 2016-08-04
+ * Time: 오전 8:40
+ * Author: hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class ChildImpl extends ParentImpl implements Child {
+}
