320a9a8ab60c222bae6d2af9ccbf699a54f844f3
hadeslee <hadesleeq@gmail.com>
2016-08-31 00:22:56 +0900

    refactor exercises and examples to use chapter numbering system

---
+++ src/lambda_expressions/chapter6/MovingAverage.java
@@ -0,0 +1,11 @@
+package lambda_expressions.chapter6;
+
+/**
+ * Created by hadeslee on 2016-08-31.
+ */
+public class MovingAverage {
+    /*public static void sma() {
+        IntStream.range(0, 1000)
+                 .reduce();
+    }*/
+}
