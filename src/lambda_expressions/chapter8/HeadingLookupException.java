18cfba301b4e7430e391f4661017eeb054b0fd12
hadeslee <hadesleeq@gmail.com>
2016-09-06 00:17:37 +0900

    refactor exercises and examples to use chapter numbering system

---
+++ src/lambda_expressions/chapter8/HeadingLookupException.java
@@ -0,0 +1,11 @@
+package lambda_expressions.chapter8;
+
+import java.io.IOException;
+
+/**
+ * Created by hadeslee on 2016-09-05.
+ */
+public class HeadingLookupException extends RuntimeException {
+    public HeadingLookupException(IOException e) {
+    }
+}
