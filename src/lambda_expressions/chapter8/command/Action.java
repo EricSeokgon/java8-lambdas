2c5a38a81b7524e56097a2f9f740fae341832288
hadeslee <hadesleeq@gmail.com>
2016-09-09 02:49:19 +0900

    BEGIN Action

---
+++ src/lambda_expressions/chapter8/command/Action.java
@@ -0,0 +1,12 @@
+package lambda_expressions.chapter8.command;
+
+/**
+ * Created by hadeslee on 2016-09-09.
+ */
+// BEGIN Action
+public interface Action {
+
+    public void perform();
+
+}
+// END Action
