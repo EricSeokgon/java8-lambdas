f9893c90636a168141083ee15f5431991b1f5668
hadeslee <hadesleeq@gmail.com>
2016-09-10 00:14:15 +0900

    interface Editor

---
+++ src/lambda_expressions/chapter8/command/Editor.java
@@ -0,0 +1,12 @@
+package lambda_expressions.chapter8.command;
+
+/**
+ * Created by hadeslee on 2016-09-10.
+ */
+public  interface Editor {
+    public void save();
+
+    public void open();
+
+    public void close();
+}
