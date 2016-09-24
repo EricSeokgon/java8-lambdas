da91237d42388e288b1ee3ab711b207dac1bd25a
hadeslee <hadesleeq@gmail.com>
2016-09-25 01:31:46 +0900

    refactor exercises and examples to use chapter numbering system

---
+++ src/lambda_expressions/chapter8/template_method/lambdas/Company.java
@@ -0,0 +1,20 @@
+package lambda_expressions.chapter8.template_method.lambdas;
+
+import lambda_expressions.chapter8.template_method.ApplicationDenied;
+
+/**
+ * Created by hadeslee on 2016-09-25.
+ */
+public class Company {
+    public void checkIdentity() throws ApplicationDenied {
+
+    }
+
+    public void checkProfitAndLoss() throws ApplicationDenied {
+
+    }
+
+    public void checkHistoricalDebt() throws ApplicationDenied {
+
+    }
+}
