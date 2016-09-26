3661ece917e31dd2bd7ea5a97ecff26e79821a4a
hadeslee <hadesleeq@gmail.com>
2016-09-27 00:24:31 +0900

    refactor exercises and examples to use chapter numbering system

---
+++ src/lambda_expressions/chapter8/template_method/traditional/EmployeeLoanApplication.java
@@ -0,0 +1,11 @@
+package lambda_expressions.chapter8.template_method.traditional;
+
+/**
+ * Created by hadeslee on 2016-09-27.
+ */
+public class EmployeeLoanApplication extends PersonalLoanApplication {
+    @Override
+    protected void checkIncomeHistory() {
+        // They work for us!
+    }
+}
---
+++ src/lambda_expressions/chapter8/template_method/traditional/PersonalLoanApplication.java
@@ -0,0 +1,22 @@
+package lambda_expressions.chapter8.template_method.traditional;
+
+/**
+ * Created by hadeslee on 2016-09-27.
+ */
+public class PersonalLoanApplication extends LoanApplication {
+    @Override
+    protected void checkIdentity() {
+
+    }
+
+    @Override
+    protected void checkIncomeHistory() {
+
+    }
+
+    @Override
+    protected void checkCreditHistory() {
+
+    }
+
+}
