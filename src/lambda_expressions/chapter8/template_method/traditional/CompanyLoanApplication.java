95190f8836c20ae251d539ac15c9f9f3d639ea53
hadesyi <hadesleeq@gmail.com>
2016-09-28 09:10:16 +0900

    refactor exercises and examples to use chapter numbering system

---
+++ src/lambda_expressions/chapter8/template_method/traditional/CompanyLoanApplication.java
@@ -0,0 +1,27 @@
+package lambda_expressions.chapter8.template_method.traditional;
+
+/**
+ * Project: java8-lambdas
+ * FileName: CompanyLoanApplication
+ * Date: 2016-09-28
+ * Time: 오전 9:09
+ * Author: user
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class CompanyLoanApplication extends LoanApplication{
+    @Override
+    protected void checkIdentity() {
+
+    }
+
+    @Override
+    protected void checkIncomeHistory() {
+
+    }
+
+    @Override
+    protected void checkCreditHistory() {
+
+    }
+}
